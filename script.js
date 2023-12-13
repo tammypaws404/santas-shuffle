09154829004e9f6a545f822844378c93de3837a7
Tammy <tammyle.uc@gmail.com>
2023-12-13 19:28:58 +1100

    Randomly generate items from the user's inputs

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
-function storeInput() {
-    const input1 = document.getElementById("one");
-    const input2 = document.getElementById("two");
-    const input3 = document.getElementById("three");
-    const input4 = document.getElementById("four");
-    const input5 = document.getElementById("five");
 
-    const value1 = input1.value;
-    const value2 = input2.value;
-    const value3 = input3.value;
-    const value4 = input4.value;
-    const value5 = input5.value;
+let clickStage = 1;
+let presentsArray = []
+function clickStar() {
+    // 1st click to store user inputs
+    if (clickStage == 1) {
+        const input1 = document.getElementById("one");
+        const input2 = document.getElementById("two");
+        const input3 = document.getElementById("three");
+        const input4 = document.getElementById("four");
+        const input5 = document.getElementById("five");
 
-    const presentsArray = [value1, value2, value3, value4, value5];
+        presentsArray.push(input1.value);
+        presentsArray.push(input2.value);
+        presentsArray.push(input3.value);
+        presentsArray.push(input4.value);
+        presentsArray.push(input5.value);
+
+        alert(presentsArray);
+    }
+
+    // 2nd click after user choose 5 presents. Generate random integers (20% chance of getting a present)
+    else if (clickStage == 2) {
+        let randomArray = [];
+        let presentsGotArray = [];
+        for (let i = 0; i < 5; i++) {
+            randomNum = Math.floor(Math.random() * 20);
+            if (!(randomNum in randomArray)) {
+                if (randomNum < 5) {
+                    presentsGotArray.push(presentsArray[randomNum]);
+                }
+            }
+        }
+        alert(presentsGotArray);
+    }
+
+    clickStage++;
 }
 
+// make the presents move on click
 let count = 0;
 function choosePresent(clickedId) {
     if (count < 5) {
